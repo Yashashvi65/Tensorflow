44f3463d4bbf489fde912a4f065e7a8985deb85e
Yashashvi Singh Bhadauria <58688602+Yashashvi65@users.noreply.github.com>
2020-07-18 23:57:18 +0530

    Update Fashion Equipments Classifier CNN.py

--- Fashion Equipments Classifier CNN.py
+++ Fashion Equipments Classifier CNN.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 print(tf.__version__)
-
+#load dataset
 mnist=tf.keras.datasets.fashion_mnist
 (training_images,training_labels),(test_images,test_labels)=mnist.load_data()
 training_images=training_images.reshape(60000,28,28,1)
